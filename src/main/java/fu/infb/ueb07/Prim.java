17192449885f817e6b20bdff55902b3c1213b889
Sven <sven.willrich@googlemail.com>
2015-05-28 15:12:59 +0200

    add comments

--- src/main/java/fu/infb/ueb07/Prim.java
+++ src/main/java/fu/infb/ueb07/Prim.java
@@ -1,15 +1,26 @@
 package fu.infb.ueb07;
 
 public class Prim {
+	// gibt alle Zahlen von 1 bis "until" aus, die Primzahlen sind
 	public static void printPrim(int until) {
+		// Läuft ab 1 und wird inkrementiert, bis kleiner/gleich "util" ist
 		for (int underTest = 1; underTest <= until; underTest++) {
+			// speichert, ob es sich bei der aktuellen Primzahl (underTest) um
+			// eine Primzahl handelt; default ist true
 			boolean isPrimeNumber = true;
+			// prüft für die aktuelle Zahl, ob sie eine Primzahl ist
 			for (int var = 2; var < underTest; var++) {
+				// Wenn underTest eine Primzahl ist, ist der Divisionsrest NIE
+				// 0, außer bei 1 und sich selbst
 				if (underTest % var == 0) {
+					// Ist der Divisionsrest 0, kann es keine Primzahl sein.
 					isPrimeNumber = false;
 					break;
 				}
 			}
+
+			// handelt es sich bei der aktuellen Zahl um eine Primzahl, wird sie
+			// ausgegeben
 			if (isPrimeNumber) {
 				System.out.println("is prime number: " + underTest);
 			}
