af3fff9b9b540ec61650eb1afc7d8d7033b05799
Sven <sven.willrich@googlemail.com>
2015-06-06 21:54:57 +0200

    zettel 8 finished

--- src/main/java/fu/infb/ueb08/PartialSum.java
+++ src/main/java/fu/infb/ueb08/PartialSum.java
@@ -2,11 +2,21 @@ package fu.infb.ueb08;
 
 public class PartialSum {
 
+	// berechnet die größte Teilsumme für die angegebenen Daten
 	public static int biggestPartialSum(int[] data) {
+		// Variablen zur Speicherung des aktuell höchsten Wertes
+		// (biggestPartialSum) und zur Speicherung des Zählers (tmpSum)
 		int biggestPartialSum = 0, tmpSum = 0;
+		// Durchfläuft solange, bis das Array durchlaufen ist
 		for (int i = 0; i < data.length; i++) {
+			// wenn der Zähler (tmpSum) sowie die aktuelle Zahl größer als null
+			// ist, passiere die Schleife, andernfalls setze tmpSum auf 0. Der
+			// Teilstring kann nicht mit einer negativen Zahl anfangen.
 			if (tmpSum + data[i] > 0) {
+				// summiere dann jeweils die aktuelle Zahl hinzu
 				tmpSum += data[i];
+				// wenn die aktuelle Teilsumme größer ist als die bisher größte,
+				// setze sie neu
 				if (tmpSum > biggestPartialSum) {
 					biggestPartialSum = tmpSum;
 				}
--- src/main/java/fu/infb/ueb08/Pi.java
+++ src/main/java/fu/infb/ueb08/Pi.java
@@ -1,17 +1,24 @@
 package fu.infb.ueb08;
 
 public class Pi {
+	// approximiert pi
 	public static double approximiere(int iterations) {
+		// zählt, wie oft das Ergebnis kleiner/gleich eins war
 		int isBetween = 0;
+		// durchläuft so viele Iterationen, wie angegeben
 		for (int i = 0; i < iterations; i++) {
+			// generiert zwei zufällige Zahlen mit drei Nachkommastellen, für
+			// die gilt 0 <= x <= 1
 			double dotx = Math.round(Math.random() * 1000.) / 1000.;
 			double doty = Math.round(Math.random() * 1000.) / 1000.;
 
+			// wenn kleiner/gleich eins, ist es enthalten
 			if (dotx * dotx + doty * doty <= 1) {
 				isBetween++;
 			}
 		}
 
+		// Konsolenausgabe
 		return 4 * (double) isBetween / iterations;
 	}
 }
